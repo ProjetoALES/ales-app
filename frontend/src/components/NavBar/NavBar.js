dbac01bbcec553a59d1df43304f30fac1872cb3f
vpalmerini <victorpalmerini@gmail.com>
2019-11-07 19:17:52 -0200

    Create NavBar component

---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./NavBar.module.scss";
+import { Navbar, Nav, Button } from "react-bootstrap";
+import Logo from "../../assets/images/logo-ales.svg";
+
+const NavBar = () => (
+  <Navbar collapseOnSelect expand="lg" variant="light" bg="light">
+    <Navbar.Brand href="/#home">
+      <img
+        alt="logo"
+        src={Logo}
+        width="160"
+        height="40"
+        className="d-inline-block align-top"
+      />
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className={styles.navContainer}>
+        <Nav.Link href="#" className={styles.navLink}>
+          Como Chegar
+        </Nav.Link>
+        <Nav.Link href="#" className={styles.navLink}>
+          Contato
+        </Nav.Link>
+        <Nav.Link href="#" className={styles.navLink}>
+          Matérias
+        </Nav.Link>
+        <div className={styles.buttonsContainer}>
+          <Button size="lg" variant="success">
+            Entrar
+          </Button>
+          <Button size="lg" variant="outline-primary">
+            Minha Área
+          </Button>
+          <Button size="lg" variant="outline-danger">
+            Sair
+          </Button>
+        </div>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default NavBar;
