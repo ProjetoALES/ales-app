7a7944c05a2ecad8b0270216ee85c4d539558322
vpalmerini <victorpalmerini@gmail.com>
2019-11-08 21:07:53 -0200

    feat: create WhatIs section

---
+++ frontend/src/pages/Home/Sections/WhatIs/WhatIs.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./WhatIs.module.scss";
+
+const WhatIs = () => (
+  <div className={styles.section}>
+    <div className={styles.sectionTitle}>
+      <h3>O que é o Ales?</h3>
+    </div>
+    <div className={styles.sectionDescription}>
+      <p>
+        Acreditamos que a curiosidade e a vontade de aprender são essenciais na
+        vida dos jovens, mas muitas vezes estes são suprimidos pela falta de
+        acesso às informações corretas ou são desestimulados pelo sistema de
+        ensino arcaico que ainda mantemos em nosso país. Reconhecemos que a
+        culpa disso não é das escolas. Muitas vezes faltam, em especial nas
+        públicas, as ferramentas e oportunidades para abordar temas
+        extra-curriculares, por mais importantes que eles sejam.
+      </p>
+      <p>
+        Acreditamos que a curiosidade e a vontade de aprender são essenciais na
+        vida dos jovens, mas muitas vezes estes são suprimidos pela falta de
+        acesso às informações corretas ou são desestimulados pelo sistema de
+        ensino arcaico que ainda mantemos em nosso país. Reconhecemos que a
+        culpa disso não é das escolas. Muitas vezes faltam, em especial nas
+        públicas, as ferramentas e oportunidades para abordar temas
+        extra-curriculares, por mais importantes que eles sejam.
+      </p>
+    </div>
+  </div>
+);
+
+export default WhatIs;
