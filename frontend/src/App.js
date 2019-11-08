7afd25f6dd937889b76356654179aef4d80f3e7d
vpalmerini <victorpalmerini@gmail.com>
2019-11-07 23:23:45 -0200

    Add Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Routes from "./Routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className="App">
         <NavBar />
         <Routes />
+        <Footer />
       </div>
     </Router>
   );
