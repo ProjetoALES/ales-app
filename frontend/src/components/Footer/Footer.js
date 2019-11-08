0ea2e57bbe0c8cd9bb024344f88f3d205b1203d6
vpalmerini <victorpalmerini@gmail.com>
2019-11-08 18:27:52 -0200

    fix: refactor links to remove warnings

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -7,16 +7,16 @@ import GitHubIcon from "../../assets/icons/github.svg";
 const Footer = () => (
   <div className={styles.container}>
     <h5>
-      <a href="#">Projeto ALES</a>
+      <a href="/#">Projeto ALES</a>
     </h5>
     <div className={styles.iconContainer}>
-      <a href="#">
+      <a href="/#">
         <img src={FacebookIcon} alt="facebook icon" width="30" height="30" />
       </a>
-      <a href="#">
+      <a href="/#">
         <img src={InstagramIcon} alt="instagram icon" width="30" height="30" />
       </a>
-      <a href="#">
+      <a href="/#">
         <img src={GitHubIcon} alt="github icon" width="30" height="30" />
       </a>
     </div>
