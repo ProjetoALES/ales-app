b369b769aa71434c552f6d3b325f810b999de3a8
vpalmerini <victorpalmerini@gmail.com>
2019-11-07 23:23:36 -0200

    Create Footer component

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+import FacebookIcon from "../../assets/icons/facebook.svg";
+import InstagramIcon from "../../assets/icons/instagram.svg";
+import GitHubIcon from "../../assets/icons/github.svg";
+
+const Footer = () => (
+  <div className={styles.container}>
+    <h5>
+      <a href="#">Projeto ALES</a>
+    </h5>
+    <div className={styles.iconContainer}>
+      <a href="#">
+        <img src={FacebookIcon} alt="facebook icon" width="30" height="30" />
+      </a>
+      <a href="#">
+        <img src={InstagramIcon} alt="instagram icon" width="30" height="30" />
+      </a>
+      <a href="#">
+        <img src={GitHubIcon} alt="github icon" width="30" height="30" />
+      </a>
+    </div>
+  </div>
+);
+
+export default Footer;
