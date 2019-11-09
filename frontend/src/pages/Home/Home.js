b91f74d647080add0ff3fd60d706d8d99e0cb9f2
vpalmerini <victorpalmerini@gmail.com>
2019-11-08 23:51:53 -0200

    feat: finish Home page layout

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Landing from "../../components/Landing/Landing";
+import WhatIs from "./Sections/WhatIs/WhatIs";
+import WhatDo from "./Sections/WhatDo/WhatDo";
+import AboutUs from "./Sections/AboutUs/AboutUs";
+import GetThere from "./Sections/GetThere/GetThere";
+import Contact from "./Sections/Contact/Contact";
+
+const Home = () => (
+  <div>
+    <Landing />
+    <WhatIs />
+    <WhatDo />
+    <AboutUs />
+    <GetThere />
+    <Contact />
+  </div>
+);
+
+export default Home;
