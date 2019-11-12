1a9a8e7770dc74c0ac50466e35b5673b8a51d5f4
vpalmerini <victorpalmerini@gmail.com>
2019-11-12 20:09:44 -0200

    feat: create static part of GetThere page

---
+++ frontend/src/pages/GetThere/GetThere.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./GetThere.module.scss";
+import GetThereSection from "./Sections/GetThereSection/GetThereSection";
+
+const GetThere = () => (
+  <div className={styles.container}>
+    <div className={styles.titleContainer}>
+      <div className={styles.title}>
+        <h3>Como Chegar</h3>
+      </div>
+      <div className={styles.description}>
+        <p>
+          Aqui te ajudamos a escolher o caminho mais fácil pra chegar nas nossas
+          aulas! \o/
+        </p>
+      </div>
+    </div>
+    <GetThereSection />
+  </div>
+);
+
+export default GetThere;
