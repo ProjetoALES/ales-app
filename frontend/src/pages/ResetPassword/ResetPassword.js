b802066bee05b6a554f2d86c3482d48b8c1a3294
vpalmerini <victorpalmerini@gmail.com>
2019-11-12 08:59:13 -0200

    feat: create ResetPassword page

---
+++ frontend/src/pages/ResetPassword/ResetPassword.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./ResetPassword.module.scss";
+import ResetPasswordCard from "../../components/ResetPasswordCard/ResetPasswordCard";
+
+const ResetPassword = () => (
+  <div className={styles.container}>
+    <ResetPasswordCard />
+  </div>
+);
+
+export default ResetPassword;
