bf0fe819411502e51cbeb0d6126ca4a05e0305bc
vpalmerini <victorpalmerini@gmail.com>
2019-11-13 18:30:43 -0200

    feat: create SearchRouteSection section

---
+++ frontend/src/pages/GetThere/Sections/SearchRouteSection/SearchRouteSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./SearchRouteSection.module.scss";
+import SearchRouteCard from "../../../../components/SearchRouteCard/SearchRouteCard";
+
+const SearchRouteSection = () => (
+  <div className={styles.container}>
+    <div className={styles.titleContainer}>
+      <h3>Descubra o melhor caminho</h3>
+    </div>
+    <SearchRouteCard />
+  </div>
+);
+
+export default SearchRouteSection;
