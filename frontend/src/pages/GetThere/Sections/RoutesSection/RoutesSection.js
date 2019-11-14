8eb826512e922aaccd93a583d4938559b28f2ac8
vpalmerini <victorpalmerini@gmail.com>
2019-11-13 23:06:19 -0200

    feat: create RoutesSection section

---
+++ frontend/src/pages/GetThere/Sections/RoutesSection/RoutesSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./RoutesSection.module.scss";
+import RouteCard from "../../../../components/RouteCard/RouteCard";
+
+const RoutesSection = () => (
+  <div className={styles.container}>
+    <RouteCard />
+    <RouteCard />
+    <RouteCard />
+  </div>
+);
+
+export default RoutesSection;
