83ccd4491bca741f3daecdfc645a0a0932843e7e
vpalmerini <victorpalmerini@gmail.com>
2019-11-15 21:36:08 -0200

    ref: set global margin top

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Routes from "./Routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import styles from "./App.module.scss";
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
+      <div className={styles.App}>
         <NavBar />
         <Routes />
         <Footer />
