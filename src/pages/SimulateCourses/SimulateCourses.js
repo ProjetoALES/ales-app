b4cf98b9e85f9de8a8e90e749cc3ef3114680e62
vpalmerini <victorpalmerini@gmail.com>
2019-11-16 12:12:56 -0200

    feat: create SimulateCourses page

---
+++ src/pages/SimulateCourses/SimulateCourses.js
@@ -0,0 +1,7 @@
+import React from "react";
+import SimulateCoursesHeader from "./Sections/SimulateCoursesHeader/SimulateCoursesHeader";
+import styles from "./SimulateCourses.module.scss";
+
+const SimulateCourses = () => <SimulateCoursesHeader />;
+
+export default SimulateCourses;
