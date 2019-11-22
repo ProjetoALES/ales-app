107f02bd71c66407a013a52f41c54f65b6b3e468
vpalmerini <victorpalmerini@gmail.com>
2019-11-21 23:17:11 -0200

    feat: finish full calendar basic styles

--- src/pages/SimulateCourses/Sections/SimulateCoursesSection/SimulateCoursesSection.js
+++ src/pages/SimulateCourses/Sections/SimulateCoursesSection/SimulateCoursesSection.js
@@ -1,11 +1,45 @@
 import React from "react";
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
+import Calendar from "@fullcalendar/react";
+import timeGrid from "@fullcalendar/timegrid";
+import ptBrLocale from "@fullcalendar/core/locales/pt-br";
 import styles from "./SimulateCoursesSection.module.scss";
 
 const SimulateCoursesSection = () => (
   <div className={styles.container}>
-    <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} />
+    <Calendar
+      locale={ptBrLocale}
+      defaultView="timeGridTwoDays"
+      contentHeight="auto"
+      allDaySlot={false}
+      slotDuration="00:30:00"
+      scrollTime="2:00:00"
+      slotLabelFormat={{
+        hour: "numeric",
+        minute: "2-digit",
+        omitZeroMinute: false
+      }}
+      validRange={{
+        start: "2000-01-01",
+        end: "2000-01-02"
+      }}
+      columnHeaderFormat={{
+        weekday: "long"
+      }}
+      views={{
+        timeGridTwoDays: {
+          type: "timeGrid",
+          duration: { days: 2 }
+        }
+      }}
+      plugins={[timeGrid]}
+      header={{
+        left: "",
+        center: "",
+        right: ""
+      }}
+      minTime="9:00"
+      maxTime="17:00"
+    />
   </div>
 );
 
