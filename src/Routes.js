9b0c6494a79a51c4d433d041e16c980bd6c20df6
vpalmerini <victorpalmerini@gmail.com>
2019-11-28 22:37:42 -0200

    feat: add route to ResetPasswordRequested page

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import ResetPassword from "./pages/ResetPassword/ResetPassword";
 import GetThere from "./pages/GetThere/GetThere";
+import ResetPasswordRequested from "./pages/ResetPasswordRequested/ResetPasswordRequested";
 
 const Routes = () => {
   return (
@@ -12,6 +13,11 @@ const Routes = () => {
       <Route path="/login" exact component={Login} />
       <Route path="/reset-password" exact component={ResetPassword} />
       <Route path="/get-there" exact component={GetThere} />
+      <Route
+        path="/reset-password-requested"
+        exact
+        component={ResetPasswordRequested}
+      />
     </Switch>
   );
 };
