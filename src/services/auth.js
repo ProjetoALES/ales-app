3b03c0f03740aabe870e2a7c3ba1f4de538787be
vpalmerini <victorpalmerini@gmail.com>
2019-12-03 20:45:42 -0200

    feat: add refresh token methods to axios config

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,13 +1,16 @@
 export const TOKEN_KEY = "access";
+export const REFRESH_TOKEN_KEY = "refresh";
 
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 
-export const login = token => {
+export const login = (token, refresh) => {
   localStorage.setItem(TOKEN_KEY, token);
+  localStorage.setItem(REFRESH_TOKEN_KEY, refresh);
 };
 
 export const logout = () => {
   localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(REFRESH_TOKEN_KEY);
 };
