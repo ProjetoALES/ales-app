6788a02cffa628c8006ff58afc95ca810fc962bb
vpalmerini <victorpalmerini@gmail.com>
2019-12-03 21:54:54 -0200

    feat: add axios interceptor to handle refresh token

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getToken } from "./auth";
+import { getToken, getRefreshToken, logout, setNewToken } from "./auth";
 
 const api = axios.create({
   baseURL: "http://localhost:8000/api/v1"
@@ -12,5 +12,32 @@ api.interceptors.request.use(async config => {
   }
   return config;
 });
+
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response.status !== 401) {
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
+    }
+
+    /* expired token */
+    if (error.response.code === "token_not_valid") {
+      api
+        .post("/token/refresh/", { refresh: getRefreshToken() })
+        .then(token => {
+          setNewToken(token);
+        })
+        .catch(err => {
+          Promise.reject(err);
+        });
+    } else {
+      logout();
+    }
+  }
+);
 
 export default api;
--- src/services/auth.js
+++ src/services/auth.js
@@ -4,13 +4,19 @@ export const REFRESH_TOKEN_KEY = "refresh";
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
+export const getRefreshToken = () => localStorage.getItem(REFRESH_TOKEN_KEY);
+
+export const setNewToken = token => {
+  localStorage.setItem(TOKEN_KEY, token);
+};
 
 export const login = (token, refresh) => {
   localStorage.setItem(TOKEN_KEY, token);
   localStorage.setItem(REFRESH_TOKEN_KEY, refresh);
 };
 
-export const logout = () => {
+export const logout = ({ history }) => {
   localStorage.removeItem(TOKEN_KEY);
   localStorage.removeItem(REFRESH_TOKEN_KEY);
+  history.push("/");
 };
