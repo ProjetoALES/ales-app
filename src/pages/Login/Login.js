6c438fd5a524b9e8735da0f157ed0e5634d9dd2f
vpalmerini <victorpalmerini@gmail.com>
2019-12-03 20:46:23 -0200

    feat: integrate login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import Alessauro from "assets/images/alessauro.png";
+import { Form, Button } from "react-bootstrap";
 import styles from "./Login.module.scss";
-import LoginCard from "components/LoginCard/LoginCard.js";
+import api from "services/api";
+import { login } from "services/auth";
 
-const Login = () => (
-  <div className={styles.container}>
-    <LoginCard />
-  </div>
-);
+const Login = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitLogin = async e => {
+    e.preventDefault();
+    if (!email || !password) {
+      alert("Preencha corretamente os campos!");
+      return;
+    }
+    try {
+      const response = await api.post("/token/", { email, password });
+      login(response.data.access, response.data.refresh);
+      history.push("/dashboard");
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardContainer}>
+        <div>
+          <img src={Alessauro} width="110" height="120" alt="ales-mascote" />
+        </div>
+        <form className={styles.formContainer} onSubmit={submitLogin}>
+          <div className={styles.formGroup}>
+            <Form.Control
+              type="email"
+              placeholder="Email"
+              onChange={e => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <Form.Control
+              type="password"
+              placeholder="Senha"
+              onChange={e => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <Button variant="success" type="submit" size="lg">
+            Entrar
+          </Button>
+          <div>
+            <a href="/reset-password">Esqueci a senha</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
