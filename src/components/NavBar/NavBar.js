cbaa4eaac318bfd95e3a5fd7ffb19460fc330805
vpalmerini <victorpalmerini@gmail.com>
2019-12-03 20:59:38 -0200

    feat: redirect to home on logout

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,54 +1,63 @@
 import React from "react";
+import { logout } from "services/auth";
 import styles from "./NavBar.module.scss";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Button } from "react-bootstrap";
 import Logo from "assets/images/logo-ales.svg";
 
-const NavBar = () => (
-  <Navbar
-    collapseOnSelect
-    expand="xl"
-    variant="light"
-    bg="light"
-    className="fixed-top"
-  >
-    <Navbar.Brand href="/">
-      <img
-        alt="logo"
-        src={Logo}
-        width="160"
-        height="40"
-        className="d-inline-block align-top"
-      />
-    </Navbar.Brand>
-    <Navbar.Toggle />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className={styles.navContainer}>
-        <Nav.Link href="/get-there" className={styles.navLink}>
-          Como Chegar
-        </Nav.Link>
-        <Nav.Link href="/#contact" className={styles.navLink}>
-          Contato
-        </Nav.Link>
-        <Nav.Link href="#" className={styles.navLink}>
-          Matérias
-        </Nav.Link>
-        <div className={styles.buttonsContainer}>
-          <Link to="/login">
-            <Button size="lg" variant="success">
-              Entrar
+const NavBar = () => {
+  const logoutUser = () => {
+    logout();
+  };
+
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="xl"
+      variant="light"
+      bg="light"
+      className="fixed-top"
+    >
+      <Navbar.Brand href="/">
+        <img
+          alt="logo"
+          src={Logo}
+          width="160"
+          height="40"
+          className="d-inline-block align-top"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className={styles.navContainer}>
+          <Nav.Link href="/get-there" className={styles.navLink}>
+            Como Chegar
+          </Nav.Link>
+          <Nav.Link href="/#contact" className={styles.navLink}>
+            Contato
+          </Nav.Link>
+          <Nav.Link href="#" className={styles.navLink}>
+            Matérias
+          </Nav.Link>
+          <div className={styles.buttonsContainer}>
+            <Link to="/login">
+              <Button size="lg" variant="success">
+                Entrar
+              </Button>
+            </Link>
+            <Button size="lg" variant="primary">
+              Minha Área
             </Button>
-          </Link>
-          <Button size="lg" variant="primary">
-            Minha Área
-          </Button>
-          <Button size="md" variant="outline-danger">
-            Sair
-          </Button>
-        </div>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+            <Link to="/">
+              <Button size="md" variant="outline-danger" onClick={logoutUser}>
+                Sair
+              </Button>
+            </Link>
+          </div>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default NavBar;
